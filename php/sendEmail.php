bb93d4b347794775c584c251dd175fbfb1396236
dustinjasen <65975850+dustinjasen@users.noreply.github.com>
2021-09-27 16:59:49 +0100

    Update sendEmail.php

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -15,10 +15,12 @@ require '../php/PHPMailer/src/SMTP.php';
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
+$noReply = 'no-reply@dustinsmith.tech';
 $toMe = 'mail@dustinsmith.tech';
-$from = 'mail@dustinsmith.tech';
-$fromWho = $_POST["email"];
+
+$fromWho = $_POST["fullname"];//'Dustin';
 $message = $_POST["message"];
+$ccSender = $_POST["email"];
 
 try {
     //Server settings
@@ -26,18 +28,18 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = $toMe;                     //SMTP username
-    $mail->Password   = 'DtechMail090921!';                               //SMTP password
+    $mail->Username   = $noReply;                     //SMTP username
+    $mail->Password   = 'noReplyDtech090921!';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom($from, $fromWho);
-    $mail->addAddress($toMe, 'From Website');     //Add a recipient
-    //$mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->setFrom($noReply, $fromWho);
+    $mail->addAddress($noReply, 'Dustin');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');  //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC($_REQUEST["email"]);
-    $mail->addBCC('dustinjasen@gmail.com');
+    $mail->addCC($ccSender);
+    $mail->addBCC($toMe);
 
     //Attachments
     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -47,7 +49,7 @@ try {
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Message from dustinsmith.tech';
     $mail->Body    = $message;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->AltBody = $message;
 
     $mail->send();
     echo 'Message has been sent';
