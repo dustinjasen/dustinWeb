668f8aba09c4ea05419503bfdc0d3705c35a0112
dustinjasen <65975850+dustinjasen@users.noreply.github.com>
2021-09-27 16:39:58 +0100

    fail send email notification

--- js/ourJs.js
+++ js/ourJs.js
@@ -1,10 +1,10 @@
 $('#contactForm').on('submit', function (e) {
-e.preventDefault();
+  e.preventDefault();
 
-var fullname = $('#fullname').val();
-var email = $('#email').val();
-var message = $('#message').val();
-  
+  var fullname = $('#fullname').val();
+  var email = $('#email').val();
+  var message = $('#message').val();
+
 
   $.ajax({
     url: './php/sendEmail.php',
@@ -14,12 +14,8 @@ var message = $('#message').val();
       email: email,
       message: message,
     },
-    
-    success: function () {
-      
-      
 
-      
+    success: function () {
       let divOne = "<div class='successBox'>";
       let contentSuccessIcon = "<p id='successIcon'><i class='far fa-check-circle'></i></p>";
       let contentSuccessText = "<p id='successText'>Sent!</p>";
@@ -28,12 +24,17 @@ var message = $('#message').val();
 
       $('.contact-form').html(successScreen);
 
-      
-      console.log('sent')
-
     },
     error: function (jqXHR, textStatus, errorThrown) {
-      alert(':' + textStatus + ' : ' + errorThrown + ' || Please press F12 to access Network Log for further info');
+let divOne = "<div class='successBox'>";
+      let contentfailIcon = "<p id='failIcon'><i class='far fa-times-circle'></i></p>";
+      let contentfailText = "<p id='failText'>Failed to Send</p>";
+      let divOneExit = "</div>";
+      let failScreen = $(divOne + contentfailIcon + contentfailText + divOneExit).fadeOut(2500);
+
+     $('.contact-form').html(failScreen);
+      
+      console.log(':' + textStatus + ' : ' + errorThrown + ' || Please press F12 to access Network Log for further info');
     },
   });
 })
